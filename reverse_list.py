d75e299ad2322b311f8309dc5b0b66a5b389e708
Aubrey Floyd <aubreyfloyd2@gmail.com>
2023-02-22 11:48:08 -0600

    Initial commit

---
+++ reverse_list.py
@@ -0,0 +1,20 @@
+# Author: Aubrey Floyd
+# GitHub username: aubreyfloyd2
+# Date: 2/22/2023
+# Description: Function that takes a list and reverses the order of the elements in the list
+#              without using slices.
+
+def reverse_list(vals):
+    """Returns the values of the list in reverse order."""
+    i = 0
+    j = len(vals)-1 # index length of list minus 1
+    while(i<j):
+        num = vals[i]
+        vals[i] = vals[j]
+        vals[j] = num
+        i+=1
+        j-=1
+
+# vals = [7, -3, 12, 9]
+# reverse_list(vals)
+# print(vals)
